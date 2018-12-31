b20d7db0daf61cc0a866664d112ef7ba5c3b58ec
benlswift <ben.swift2016@my.ntu.ac.uk>
2018-12-31 12:27:53 +0000

    version 1 complete

---
+++ Rename/upload.sh
@@ -0,0 +1,3 @@
+curl -F mp3=@../../Logged\ Audio/2018/12/31/1100-FLYFM-31122018.mp3 \
+	 -F "name=Emily Webb 31\12" \
+	 https://api.mixcloud.com//upload/?access_token=qKz48A8ht5UQZsrK5kWVtLr59ppvsxF7
